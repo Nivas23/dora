b0599e7d36474157ed0172c35db6b5bd51225074
Nivas <tridentniva001@gmail.com>
2024-03-22 13:59:51 +0530

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "794de56f9d305179"; }
+/******/ 		__webpack_require__.h = function() { return "8f786b18ce58eafd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
